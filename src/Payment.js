9cf694d26abda8a1c589b4c0b74fb42791586c90
Emma Van der Haeghen <91066122+pgm-emmavanderhaeghen@users.noreply.github.com>
2022-05-18 17:47:35 +0200

    Create payment page

---
+++ src/Payment.js
@@ -0,0 +1,13 @@
+import styles from "./assets/styles/App.module.scss";
+
+function Payment() {
+  return (
+    <div className={styles.container}>      
+      <div className={styles.m4}>
+        <h1>Payment page</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Payment;
