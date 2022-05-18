1dd22aa3b75a623eee99e78921bf7f350cd07336
Emma Van der Haeghen <91066122+pgm-emmavanderhaeghen@users.noreply.github.com>
2022-05-18 17:47:13 +0200

    Create summary page

---
+++ src/Summary.js
@@ -0,0 +1,13 @@
+import styles from "./assets/styles/App.module.scss";
+
+function Summary() {
+  return (
+    <div className={styles.container}>      
+      <div className={styles.m4}>
+        <h1>Summary page</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Summary;
