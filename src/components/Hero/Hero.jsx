c2af61841abbb5a0ba362132f74c71fa6d650c42
Emma Van der Haeghen <91066122+pgm-emmavanderhaeghen@users.noreply.github.com>
2022-05-18 17:45:26 +0200

    Created Hero component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import hero from "../../assets/images/Hero-banner-text.png"
+import styles from "./Hero.module.scss"
+
+const Hero = () => {
+  return (
+    <img src={hero} className={styles.logo} alt="logo" />
+
+  )
+}
+
+export default Hero
