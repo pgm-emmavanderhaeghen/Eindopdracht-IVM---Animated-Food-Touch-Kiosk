0ebf7a7b0e7310439c72ca2d84ac6d2f3551e596
Emma Van der Haeghen <91066122+pgm-emmavanderhaeghen@users.noreply.github.com>
2022-05-24 19:28:23 +0200

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import Start from "./Start";
-import Selection from "./Selection";
-import Summary from "./Summary";
-import Payment from "./Payment";
-import NotFound from "./NotFound";
+import Start from "./pages/Start";
+import Selection from "./pages/Selection";
+import Summary from "./pages/Summary";
+import Payment from "./pages/Payment";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Start />} />
-        <Route path="selection" element={<Selection />} />
-        <Route path="summary" element={<Summary />} />
-        <Route path="payment" element={<Payment />} />
+        <Route path="/selection" element={<Selection />} />
+        <Route path="/summary" element={<Summary />} />
+        <Route path="/payment" element={<Payment />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
