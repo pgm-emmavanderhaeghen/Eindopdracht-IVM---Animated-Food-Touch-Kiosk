0378b2e55fca75bb74dce3ac86d65d866975725b
Emma Van der Haeghen <91066122+pgm-emmavanderhaeghen@users.noreply.github.com>
2022-05-30 13:03:30 +0200

    update sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { routes } from '../../pages/routes'
+import Button from '../Button/Button'
 import styles from "./Sidebar.module.scss"
 
 const Sidebar = ({children}) => {
   return (
     <div className={styles.sidebar}>
-        {children}
+        <h2>My order</h2>
+        <h3>Take away</h3>
+        <div className={styles.scrollbar}>
+            {children}
+        </div>
+        <Link to={routes.SUMMARY}>
+            <Button color='red'>
+              Let's order!<br/>         
+            </Button>
+        </Link>
     </div>
   )
 }
