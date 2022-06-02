7137ce110c08d9244f1fbb118f47adf1a9215eab
Emma Van der Haeghen <91066122+pgm-emmavanderhaeghen@users.noreply.github.com>
2022-06-02 15:53:01 +0200

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 import { ROUTES } from '../../constans/routes'
 import Button from '../Button/Button'
 import styles from "./Sidebar.module.scss"
 
 const Sidebar = ({title = 'My order', children}) => {
-  return (
-    <div className={styles.sidebar}>
-        <h2>{title}</h2>
-        <h3>Take away</h3>
-        <div className={styles.scrollbar}>
-            {children}
+
+    return (
+        <div className={styles.sidebar}>
+            <h2>{title}</h2>
+            <h3>Take away</h3>
+            <div className={styles.scrollbar}>
+                {children}
+            </div>
+            <Link to={ROUTES.SUMMARY}>
+                <Button color='red'>
+                Let's order!<br/>         
+                </Button>
+            </Link>
         </div>
-        <Link to={ROUTES.SUMMARY}>
-            <Button color='red'>
-              Let's order!<br/>         
-            </Button>
-        </Link>
-    </div>
-  )
+    )
 }
 
 export default Sidebar
