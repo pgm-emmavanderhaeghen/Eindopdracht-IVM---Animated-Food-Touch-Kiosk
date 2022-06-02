fc0b35870e361ea84332f0038e6e1a3d15ee9db2
Emma Van der Haeghen <91066122+pgm-emmavanderhaeghen@users.noreply.github.com>
2022-06-02 15:52:55 +0200

    Create Counter.jsx

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import Button from '../Button/Button'
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+    const handleClick = (a) => {
+        setCount(count + a)
+    }
+
+    return (
+        <div>
+            <Button onClick={() => handleClick(-1)} variant="small" color="pink">
+                -
+            </Button>
+            {count}
+            <Button onClick={() => handleClick(1)} variant="small" color="pink">
+                +
+            </Button>
+        </div>
+    )
+}
+
+export default Counter
