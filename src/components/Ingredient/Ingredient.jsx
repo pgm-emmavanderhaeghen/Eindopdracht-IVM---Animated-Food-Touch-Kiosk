1d94becb5e5ef90f58821808687ceaafdddf2e88
Emma Van der Haeghen <91066122+pgm-emmavanderhaeghen@users.noreply.github.com>
2022-06-06 22:01:45 +0200

    added ingredient component

---
+++ src/components/Ingredient/Ingredient.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import order from "../../assets/images/Boccabeker-p-500.png";
+
+export const Ingredient = () => {
+  return (
+    <div>
+        <img src={order} className={styles.orderImg} alt="order" />
+        small
+    </div>
+  )
+}
