3f3ee27dfbe69367da2adfa3026cc75f17093fe1
Emma Van der Haeghen <91066122+pgm-emmavanderhaeghen@users.noreply.github.com>
2022-06-07 14:47:49 +0200

    Added gif to payment page

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -2,7 +2,7 @@ import styles from "../App.module.scss";
 import { Link } from "react-router-dom";
 import Button from "../components/Button/Button";
 
-import loading from "../assets/images/loading.svg"
+import loading from "../assets/images/order-being-prepared.gif"
 import { ROUTES } from "../constans/routes";
 
 function Payment() {
@@ -10,8 +10,8 @@ function Payment() {
     <div className={styles.container}>      
       <div className={styles.m4}>
         <h1>Almost done!</h1>
-        <img src={loading} alt="order" />
-        <p>Please follow the instructions displayed on the credit card terminal below</p>
+        <img className={styles.round} src={loading} alt="order" />
+        <p>Please follow the instructions displayed <br></br> on the credit card terminal below</p>
         <Link to={ROUTES.SUMMARY}>
             <Button>
               Go back <br/>         
