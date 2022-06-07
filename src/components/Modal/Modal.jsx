e4a285c5624b953c36cc02edf71952af4acdf5a0
Emma Van der Haeghen <91066122+pgm-emmavanderhaeghen@users.noreply.github.com>
2022-06-07 14:48:27 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Button from '../Button/Button'
 import styles from "./Modal.module.scss"
 
-import order from "../../assets/images/Boccabeker-p-500.png";
-import useFetch from '../../hooks/useFetch';
-import { TODO_API } from '../../constans/api';
+// import useFetch from '../../hooks/useFetch';
+// import { TODO_API } from '../../constans/api';
+import IngredientList from '../IngredientList/IngredientList';
+import { Ingredient } from '../Ingredient/Ingredient';
 
 const Modal = ({show, onClose, children}) => {
 
-    const [response] = useFetch(TODO_API);
+    // const [response] = useFetch(TODO_API);
 
     if (!show) {
         return null
@@ -18,81 +19,10 @@ const Modal = ({show, onClose, children}) => {
         <div className={styles.modal} onClick={onClose}>
             <div className={styles.modalContent} onClick={e => e.stopPropagation()}>
                 <h2>Pasta in 4 steps</h2>
-                <h4>Size</h4>
-                    <Button variant="small">
-                        <img src={order} className={styles.orderImg} alt="order" />
-                        small
-                    </Button>
-                    <Button variant="small">
-                        <img src={order} className={styles.orderImg} alt="order" />
-                        medium
-                    </Button>
-                    <Button variant="small">
-                        <img src={order} className={styles.orderImg} alt="order" />
-                        large
-                    </Button>
-                <h4>Pasta</h4>
-                    <Button variant="small">
-                        <img src={order} className={styles.orderImg} alt="order" />
-                        penne
-                    </Button>
-                    <Button variant="small">
-                        <img src={order} className={styles.orderImg} alt="order" />
-                        spaghetti
-                    </Button>
-                    <Button variant="small">
-                        <img src={order} className={styles.orderImg} alt="order" />
-                        fusilli
-                    </Button>
-                    <Button variant="small">
-                        <img src={order} className={styles.orderImg} alt="order" />
-                        gluten-free
-                    </Button>
-                <h4>Sauce</h4>
-                    <Button variant="small">
-                        <img src={order} className={styles.orderImg} alt="order" />
-                        bocca
-                    </Button>
-                    <Button variant="small">
-                        <img src={order} className={styles.orderImg} alt="order" />
-                        bolognese
-                    </Button>
-                    <Button variant="small">
-                        <img src={order} className={styles.orderImg} alt="order" />
-                        cheese <br></br> & ham
-                    </Button>
-                    <Button variant="small">
-                        <img src={order} className={styles.orderImg} alt="order" />
-                        veggie
-                    </Button>
-                    <Button variant="small">
-                        <img src={order} className={styles.orderImg} alt="order" />
-                        marisol
-                    </Button>
-                    <Button variant="small">
-                        <img src={order} className={styles.orderImg} alt="order" />
-                        4 cheeses
-                    </Button>
-                    <Button variant="small">
-                        <img src={order} className={styles.orderImg} alt="order" />
-                        currysauce
-                    </Button>
-                    <Button variant="small">
-                        <img src={order} className={styles.orderImg} alt="order" />
-                        arrabbiata
-                    </Button>
-                    <Button variant="small">
-                        <img src={order} className={styles.orderImg} alt="order" />
-                        green pesto
-                    </Button>
-                    <Button variant="small">
-                        <img src={order} className={styles.orderImg} alt="order" />
-                        red pesto
-                    </Button>
-                <h4>Toppings</h4>
-                <p>{JSON.stringify(response.pasta.toppings)}</p>
-
-                <button onClick={onClose} className={styles.button}>Cancel</button>
+                <IngredientList>
+                    <Ingredient></Ingredient>
+                </IngredientList>
+                <Button onClick={onClose} color='red' className={styles.button}>Put it on the list!</Button>
             </div>
         </div>
     )
