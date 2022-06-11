f8ef1cef95de51c82716f85466ada73185dd2c13
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-06-11 10:08:21 +0200

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,11 +3,11 @@ import styles from "./Button.module.scss"
 
 
 const Button = ({color= '', variant= '', children, onClick}) => {
-  return (
-    <button onClick={onClick} className={`${styles.btn} ${styles[variant]} ${styles[color]}`}>
-        {children}
-    </button>
-  )
+    return (
+        <button onClick={onClick} className={`${styles.btn} ${styles[variant]} ${styles[color]}`}>
+            {children}
+        </button>
+    )
 }
 
 export default Button
