b130a157fedb0dcc788ae31b14d082de96fba758
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-06-11 10:08:24 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import Button from '../Button/Button'
 import styles from "./Modal.module.scss"
 
-import useFetch from '../../hooks/useFetch';
-import { TODO_API } from '../../constants/api';
-import IngredientList from '../IngredientList/IngredientList';
-import { Ingredient } from '../Ingredient/Ingredient';
+// import useFetch from '../../hooks/useFetch';
+// import { TODO_API } from '../../constants/api';
+// import IngredientList from '../IngredientList/IngredientList';
+// import { Ingredient } from '../Ingredient/Ingredient';
 
 const Modal = ({data, onClose, children}) => {
-    console.log(data)
     return (
         <div className={styles.modal} onClick={onClose}>
             <div className={styles.modalContent} onClick={e => e.stopPropagation()}>
