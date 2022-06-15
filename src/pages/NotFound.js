8554b07554e6344648e0bd66b3ce71cbb4d25219
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-06-15 15:45:59 +0200

    Update NotFound.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,19 +2,26 @@ import styles from "../App.module.scss";
 import Button from "../components/Button/Button";
 import { Link } from "react-router-dom";
 import Heading from "../components/Heading/Heading";
+import notFound from "../assets/images/notfound.gif"
 
 function NotFound() {
   return (
     <div className={styles.container}>      
       <div className={styles.m4}>
 
-      <Heading number="1">Oops!</Heading>
-      <Heading number="2" color="red">The page you were looking for doesn't exist</Heading>
-      <p>Maybe the page you were looking for is moved or deleted. </p>
+        <Heading number="1">Oops!</Heading>
+        <Heading number="2" color="red">The page you were <br></br> looking  for doesn't exist</Heading>
 
-        <Button color='red'>
-          <Link to="/"> Go to the start page</Link>
-        </Button>
+        <img className={styles.round} src={notFound} alt="order" />
+
+        <p>Maybe the page you were looking for is moved or deleted. </p>
+
+        <Link to="/">
+          <Button color='red'>
+            Go to the start page
+          </Button>
+        </Link>
+        
       </div>
     </div>
   );
