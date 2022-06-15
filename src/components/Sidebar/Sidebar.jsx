011e74ec22f13b993ceff8e2f3a3f373acfaa613
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-06-15 15:46:11 +0200

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { ROUTES } from '../../constants/routes'
 import Button from '../Button/Button'
+import Heading from '../Heading/Heading'
 import styles from "./Sidebar.module.scss"
 
 const Sidebar = ({title = 'My order', children}) => {
 
     return (
         <div className={styles.sidebar}>
-            <h2>{title}</h2>
-            <h3>Take away</h3>
+
+            <Heading number="2">{title}</Heading>
+            <Heading number="3">Take away</Heading>
+
             <div className={styles.scrollbar}>
                 {children}
             </div>
+
             <Link to={ROUTES.SUMMARY}>
                 <Button color='red'>
                 Let's order!<br/>         
