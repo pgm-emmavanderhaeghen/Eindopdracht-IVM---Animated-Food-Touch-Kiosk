d86eeb9cd6550ed326770191017753ad11822a96
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-08-16 17:42:48 +0200

    animated route

--- src/pages/Failure.js
+++ src/pages/Failure.js
@@ -1,10 +1,32 @@
 import styles from "../App.module.scss";
 import Heading from "../components/Heading/Heading";
 import failure from "../assets/images/failure.gif"
+import {motion} from 'framer-motion'
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    y: -200
+  },
+  visible: {
+    opacity: 1,
+    y: -10,
+  },
+  exit: {
+    opacity: 0,
+    y: -200,
+    transition: { ease: 'easeInOut'}
+  }
+}
 
 function Failure() {
   return (
-    <div className={styles.container}>      
+    <motion.div 
+    variants={containerVariants}
+    initial="hidden"
+    animate="visible"
+    exit="exit"
+    className={styles.container}>      
       <div className={styles.m4}>
 
         <Heading number="1">Oops, something <br></br> went wrong!</Heading>
@@ -15,7 +37,7 @@ function Failure() {
         <img className={styles.round} src={failure} alt="failure" />
 
       </div>
-    </div>
+    </motion.div>
   );
 }
 
