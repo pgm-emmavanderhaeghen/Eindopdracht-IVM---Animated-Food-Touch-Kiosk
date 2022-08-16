11dd91177b2f418c6ae9847ef57d302b2e0234be
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-08-16 17:42:15 +0200

    added Context and AnimatePresence

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { ROUTES } from "./constants/routes";
 import Start from "./pages/Start";
 import Selection from "./pages/Selection";
 import Summary from "./pages/Summary";
 import Payment from "./pages/Payment";
 import NotFound from "./pages/NotFound";
-
 import Success from "./pages/Success";
 import Failure from "./pages/Failure";
+import { createContext, useState } from "react";
+import { AnimatePresence } from "framer-motion"
+export const OrderContext = createContext();
 
 function App() {
+  const location = useLocation();
+  const [order, setOrder] = useState({});
+
   return (
-    <div className="App">
-      <Routes>
-        <Route path={ROUTES.START} element={<Start />} />
-        <Route path={ROUTES.SELECTION} element={<Selection />} />
-        <Route path={ROUTES.SUMMARY} element={<Summary />} />
-        <Route path={ROUTES.PAYMENT} element={<Payment />} />
-        <Route path={ROUTES.SUCCESS} element={<Success />} />
-        <Route path={ROUTES.FAILURE} element={<Failure />} />
-        <Route path={ROUTES.NOTFOUND} element={<NotFound />} />
-      </Routes>
-    </div>
+      <OrderContext.Provider value={[order, setOrder]}>
+        <AnimatePresence >
+        <Routes location={location} key={location.key}>
+          <Route path={ROUTES.START} element={<Start />} />
+          <Route path={ROUTES.SELECTION} element={<Selection />} />
+          <Route path={ROUTES.SUMMARY} element={<Summary />} />
+          <Route path={ROUTES.PAYMENT} element={<Payment />} />
+          <Route path={ROUTES.SUCCESS} element={<Success />} />
+          <Route path={ROUTES.FAILURE} element={<Failure />} />
+          <Route path={ROUTES.NOTFOUND} element={<NotFound />} />
+        </Routes>
+        </AnimatePresence>
+      </OrderContext.Provider>
   );
 }
 
