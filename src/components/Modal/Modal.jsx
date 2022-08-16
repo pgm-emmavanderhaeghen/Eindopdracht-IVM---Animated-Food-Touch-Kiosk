71d2ae3a2e582851e65466a75abc99eb636f0300
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-08-16 17:44:21 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import Button from '../Button/Button'
 import { Ingredient } from '../Ingredient/Ingredient';
 import styles from "./Modal.module.scss"
-// import IngredientList from '../IngredientList/IngredientList';
-import order from "../../assets/images/Boccabeker-p-500.png";
 import Heading from '../Heading/Heading';
+// import {motion} from 'framer-motion';
 
 
 const Modal = ({data, onClose, children}) => {
@@ -19,11 +18,14 @@ const Modal = ({data, onClose, children}) => {
     //   </ul>
     //   )
 
-    console.log(data)
     return (
-        <div className={styles.modal} onClick={onClose}>
+        <div 
+        className={styles.modal} 
+        onClick={onClose}>
 
-            <div className={styles.modalContent} onClick={e => e.stopPropagation()}>
+            <div 
+            className={styles.modalContent} 
+            onClick={e => e.stopPropagation()}>
             {data && typeof data === 'object' ?
                 Object.keys(data)?.map(subcat => (
                     <>
