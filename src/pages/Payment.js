b2cde090b13f07d7d241deaebc8646adb2c33901
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-08-16 17:42:55 +0200

    animated route

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,10 +1,32 @@
 import styles from "../App.module.scss";
 import loading from "../assets/images/order-being-prepared.gif"
 import Heading from "../components/Heading/Heading";
+import {motion} from 'framer-motion'
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    y: -200
+  },
+  visible: {
+    opacity: 1,
+    y: -10,
+  },
+  exit: {
+    opacity: 0,
+    y: -200,
+    transition: { ease: 'easeInOut'}
+  }
+}
 
 function Payment() {
   return (
-    <div className={styles.container}>      
+    <motion.div 
+    variants={containerVariants}
+    initial="hidden"
+    animate="visible"
+    exit="exit"
+    className={styles.container}>      
       <div className={styles.m4}>
 
         <Heading number="1">Almost done!</Heading>
@@ -13,7 +35,7 @@ function Payment() {
         <p>Please follow the instructions displayed <br></br> on the credit card terminal below</p>
         
       </div>
-    </div>
+    </motion.div>
   );
 }
 
