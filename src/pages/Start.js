b7faaaeb5694bf05f14345aae352a32b85563c9b
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-08-16 17:43:06 +0200

    animated route

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -5,23 +5,55 @@ import Hero from "../components/Hero/Hero";
 import Button from "../components/Button/Button"
 import { ROUTES } from "../constants/routes";
 import Heading from "../components/Heading/Heading";
+import {motion} from 'framer-motion';
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    y: -200
+  },
+  visible: {
+    opacity: 1,
+    y: -10,
+  },
+  exit: {
+    opacity: 0,
+    y: -200,
+    transition: { ease: 'easeInOut'}
+  }
+}
 
 function Start() {
   return (
-    <div className={styles.container}>  
+    <motion.div 
+    variants={containerVariants}
+    initial="hidden"
+    animate="visible"
+    exit="exit"
+    className={styles.container}
+    >  
 
     <Hero />
 
-      <div className={styles.m4}>
+      <motion.div className={styles.m4}
+      animate= {{ marginTop: 20 }}
+      >
 
         <Heading number="1">Welcome at bocca</Heading>
         <p className={styles.text} >
-          Bocca is a fresh, quick and delicious <strong>pastabar </strong>  
+          Bocca is a fresh, quick and delicious <motion.strong animate={{color: '#DF2027'}}> pastabar </motion.strong>  
           located in Bruges & Ghent. Check out what we got on the menu.
         </p>
-        <Heading number="2" color="red" variant="spaced">Hungry?</Heading>
+
+        <motion.p animate={{
+          fontSize: '2.5rem', 
+          color:'#DF2027', 
+          marginTop: '2rem',
+          fontWeight: 700}}>
+          Hungry?
+        </motion.p>
       
-      </div>
+      </motion.div>
 
       <div className={styles.m4}>
 
@@ -46,7 +78,7 @@ function Start() {
         </Link>   
               
       </div>
-    </div>
+    </motion.div>
   );
 }
 
