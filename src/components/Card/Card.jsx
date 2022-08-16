60c3b2eef457ef28fd786494b779a2066038647e
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-08-16 17:43:57 +0200

    updated card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from "./Card.module.scss"
+import Counter from '../Counter/Counter';
 
-const Card = ({color= '', variant= '', children}) => {
+const Card = ({item, color= '', variant= '', children}) => {
   return (
     <div className={`${styles.card} ${styles[variant]} ${styles[color]}`}>
         {children}
+        <Counter item={item}/>
     </div>
   )
 }
