88d1f6c692a381d55e8e8b4e4887d0ca30890c98
pgm-emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2022-08-16 17:44:19 +0200

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import hero from "../../assets/images/Hero-banner-text.png"
 import styles from "./Hero.module.scss"
+import {motion} from 'framer-motion'
 
 const Hero = () => {
   return (
-    <img 
+    <motion.img 
+    initial= {{y: -250}}
+    animate= {{ y: -10}}
     src={hero} 
     className={styles.logo} 
     alt="logo" />
